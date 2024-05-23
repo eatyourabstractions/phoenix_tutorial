f497f32fdfe6daeaa02620b820b5bc26519018c3
Michael Matos <mbaezmichael@gmail.com>
2024-05-22 18:20:55 -0700

    second commit

---
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -0,0 +1,17 @@
+defmodule HelloWeb.PageController do
+  use HelloWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def hello(conn, _params) do
+    html(conn, "hello, world!")
+  end
+
+  def goodbye(conn, _params) do
+    html(conn, "goodbye, world!")
+  end
+end
