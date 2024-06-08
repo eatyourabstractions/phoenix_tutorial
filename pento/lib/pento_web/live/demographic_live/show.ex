d634754f1d66e70758550016eee41b5d85e5c712
Michael Matos <mbaezmichael@gmail.com>
2024-06-07 18:20:50 -0700

    how do tables works in liveview lesson

--- pento/lib/pento_web/live/demographic_live/show.ex
+++ pento/lib/pento_web/live/demographic_live/show.ex
@@ -3,16 +3,21 @@ defmodule PentoWeb.DemographicLive.Show do
   use Phoenix.Component
 
   alias Pento.Survey.Demographic
+  alias PentoWeb.CoreComponents
 
   attr :demographic, Demographic, required: true
   def details(assigns) do
     ~H"""
       <div>
         <h2 class="font-medium text 2xl">Demographics <span>&#x2713</span></h2>
-        <ul>
-          <li>Gender: <%= @demographic.gender %></li>
-          <li>Year of birth: <%= @demographic.year_of_birth %></li>
-        </ul>
+          <CoreComponents.table rows={[@demographic]} id="demographic-table">
+            <:col :let={demographic} label="Gender">
+              <%= demographic.gender %>
+            </:col>
+            <:col :let={demographic} label="Year Of Birth">
+              <%= demographic.year_of_birth %>
+            </:col>
+          </CoreComponents.table>
       </div>
     """
   end
