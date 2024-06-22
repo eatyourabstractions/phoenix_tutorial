3b9de44a6ff30e9f2b08deb007544d47ff3b1d6e
Michael Matos <mbaezmichael@gmail.com>
2024-06-21 17:21:57 -0700

    need to fix rating liveview component

--- pento/lib/pento_web/live/survey_live.ex
+++ pento/lib/pento_web/live/survey_live.ex
@@ -1,15 +1,27 @@
 
 defmodule PentoWeb.SurveyLive do
   # use
+  alias Pento.Catalog
   use PentoWeb, :live_view
 
   # aliases
   alias Pento.Survey
   alias PentoWeb.DemographicLive
+  # alias PentoWeb.RatingLive.Show
+  alias PentoWeb.RatingLive
   # alias __MODULE__.Component
 
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign_demographic }
+    {:ok, socket |> assign_demographic |> assign_products }
+  end
+
+  defp assign_products(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :products, list_products(current_user))
+  end
+
+  defp list_products(user) do
+    Catalog.list_products_with_user_ratings(user)
+
   end
 
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
@@ -23,10 +35,23 @@ defmodule PentoWeb.SurveyLive do
 
   end
 
+  def handle_info({:created_rating, updated_product, product_index}, socket) do
+    {:noreply, handle_rating_created(socket, updated_product, product_index)}
+
+  end
+
   def handle_demographic_created(socket, demographic) do
     socket
       |> put_flash(:info, "Demographic created succesfully")
       |> assign(:demographic, demographic)
   end
 
+
+  def handle_rating_created(%{assigns: %{products: products}} = socket, updated_product, product_index) do
+    IO.puts("handle_rating_created #{products}")
+    socket
+      |> put_flash(:info, "Ratting submitted succesfully")
+      |> assign(:products, List.replace_at(products, product_index, updated_product))
+
+  end
 end
--- pento/lib/pento_web/router.ex
+++ pento/lib/pento_web/router.ex
@@ -91,7 +91,7 @@ defmodule PentoWeb.Router do
   scope "/", PentoWeb do
     pipe_through [:browser]
 
-    delete "/users/log_out", UserSessionController, :delete
+    get "/users/log_out", UserSessionController, :delete
 
     live_session :current_user,
       on_mount: [{PentoWeb.UserAuth, :mount_current_user}] do
