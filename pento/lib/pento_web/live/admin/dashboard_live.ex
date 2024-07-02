27aa798bf2ffbfd5cdb01d537e4e4f03a930057a
Michael Matos <mbaezmichael@gmail.com>
2024-07-02 16:45:26 -0700

    forgot what I did

--- pento/lib/pento_web/live/admin/dashboard_live.ex
+++ pento/lib/pento_web/live/admin/dashboard_live.ex
@@ -1,21 +1,35 @@
 
 defmodule PentoWeb.Admin.DashboardLive do
   alias PentoWeb.Admin.SurveyResultsLive
+  alias PentoWeb.Admin.UserActivityLive
   use PentoWeb, :live_view
   alias PentoWeb.Endpoint
 
   @survey_results_topic "survey_results"
+  @user_activity_topic "user_activity"
 
   def mount(_params, _session, socket) do
 
     if connected?(socket) do
       Endpoint.subscribe(@survey_results_topic)
+      Endpoint.subscribe(@user_activity_topic)
     end
 
     {:ok,
       socket
       |> assign(:survey_results_component_id, "survey_results")
+      |> assign(:user_activity_component_id, "user_activity")
     }
+
+  end
+
+  def handle_info(%{event: "presence_diff"}, socket) do
+    send_update(
+      UserActivityLive,
+      id: socket.assigns.user_activity_component_id
+    )
+
+    {:noreply, socket}
 
   end
 
